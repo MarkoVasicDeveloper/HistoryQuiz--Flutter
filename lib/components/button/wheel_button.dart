d05d7653e3fc573c2a89c9330ed98bd9715749af
vasic <markovasicdeveloper@gmail.com>
2024-01-25 16:42:46 +0100

    Wheel button

---
+++ lib/components/button/wheel_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:istorija_srbije/core/shared/animations/entrance_transition.dart';
+import 'package:istorija_srbije/core/shared/animations/pulsating.dart';
+
+class WheelButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const WheelButton({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EntranceTransition(
+      width: MediaQuery.of(context).size.width * 0.2,
+      delay: const Duration(seconds: 1),
+      fadeIn: true,
+      offsetX: -100.0,
+      offsetY: 0.0,
+      initialX: 0.0,
+      initialY: 0.0,
+      offsetXPercentage: 0.06,
+      offsetYPercentage: 0.0,
+      child: Pulsating(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.2,
+          height: 20,
+          child: ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.all(1.0),
+            ),
+            child: const Text('Zavrti', style: TextStyle(fontSize: 12)),
+          ),
+        ),
+      ),
+    );
+  }
+}
